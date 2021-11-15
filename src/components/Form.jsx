2bcd41d984831d4c18a02ac0153e94cc2c36a4f3
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-15 14:25:59 +0700

    create state

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,31 +5,46 @@ const Form = () => {
     const [ username, setUsername ] = useState("");
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
-    const [ confimPassword, setConfirmPassword ] = useState("")
+    const [ confimPassword, setConfirmPassword ] = useState("");
+
+    const [ errorUsername, setErrorUsername ] = useState("");
+    const [ errorEmail, setErrorEmail ] = useState("");
+    const [ errorPassword, setErrorPassword ] = useState("");
+    const [ errorConfirmPassword, setErrorConfirmPassword] = useState("");
+
+
+    const submitForm = (e) => {
+        e.preventDefault();
+        console.log("submit");
+
+        if (username.length < 8) {
+            setErrorUsername("ป้อนชื่อผู้ใช้ จำนวนมากว่า 8 ตัวอักษร")
+        }
+    }
 
     return (
         <div className="container">
-            <form>
+            <form onSubmit={e => submitForm(e)}>
                 <h2>แบบฟอร์มลงทะเบียน</h2>
                 <div className="form-control">
                     <label>ชื่อผู้ใช้งาน</label>
-                    <input type="text" />
-                    <small>error message</small>
+                    <input type="text" onChange={(e => setUsername(e.target.value))} />
+                    <small>{errorUsername}</small>
                 </div>
                 <div className="form-control">
                     <label>อีเมล</label>
-                    <input type="text" />
-                    <small>error message</small>
+                    <input type="text" onChange={e => setEmail(e.target.value)}/>
+                    <small>{errorEmail}</small>
                 </div>
                 <div className="form-control">
                     <label>รหัสผ่าน</label>
-                    <input type="text" />
-                    <small>error message</small>
+                    <input type="password" onChange={e => setPassword(e.target.value)} />
+                    <small>{errorPassword}</small>
                 </div>
                 <div className="form-control">
                     <label>ยืนยันรหัสผ่าน</label>
-                    <input type="text" />
-                    <small>error message</small>
+                    <input type="password" onChange={e => setConfirmPassword(e.target.value)} />
+                    <small>{errorConfirmPassword}</small>
                 </div>
                 <button type="submit">ลงทะเบียน</button>
             </form>
